3d9ac4307a38345cd5519efdcf35bb5908e5e091
VJS <minsung_96@naver.com>
2021-07-03 18:12:24 +0900

    final

--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,15 @@
 import axios from 'axios'
 const url = 'https://covid19.mathdro.id/api'
 
-export const fetchData = async () => {
+export const fetchData = async (country) => {
+    let changableUrl = url
+    if (country) {
+        changableUrl = `${url}/countries/${country}`
+    }
+
     try {
         
-        const { data : { confirmed, recovered, deaths, lastUpdate } } = await axios.get(url)
+        const { data : { confirmed, recovered, deaths, lastUpdate } } = await axios.get(changableUrl)
 
         const modifiedData = {
             confirmed,
@@ -16,7 +21,7 @@ export const fetchData = async () => {
         return modifiedData
 
     } catch (error) {
-        
+        console.log(error)
     }
 }
 
@@ -32,6 +37,16 @@ export const fetchDailyData = async() => {
 
         return modifiedData
     } catch (error) {
-        
+        console.log(error)
+    }
+}
+
+export const fetchCountries = async() => {
+    try {
+        const {data : {countries}} = await axios.get(`${url}/countries`)
+
+        return countries.map((country) => country.name)
+    } catch (error) {
+        console.log(error)
     }
 }
